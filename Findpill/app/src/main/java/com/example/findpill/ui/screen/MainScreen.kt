fc3a59a2d3b02f7850315ed94e7ad9e3c1b61f09
전동원 <jdw7645@naver.com>
2025-05-14 01:14:23 +0900

    Main Screen Progress

--- Findpill/app/src/main/java/com/example/findpill/ui/screen/MainScreen.kt
+++ Findpill/app/src/main/java/com/example/findpill/ui/screen/MainScreen.kt
@@ -9,26 +9,66 @@ import androidx.compose.ui.Alignment
 import androidx.navigation.NavController
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.findpill.R
 import com.example.findpill.ui.theme.*
 
+@Preview(showBackground = true)
 @Composable
 fun MainScreen(navController: NavController){
     Column (
         modifier = Modifier
             .fillMaxSize()
-            .background(backgroundColor),
+            .background(backgroundColor).padding(horizontal = 16.dp, vertical = 24.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceBetween
+        verticalArrangement = Arrangement.spacedBy(16.dp)
     ){
-        Spacer(modifier = Modifier.height(40.dp))
+        Row( // 이미지를 End(오른쪽 정렬)로 70퍼센트 만큼 3:1의 비율(aspectRatio)로 배치
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.End
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.pill1), contentDescription = null,
+                modifier = Modifier.fillMaxWidth(0.7f),
+                contentScale = ContentScale.Crop
+            )
+        }
 
-        Image(painter = painterResource(id = R.drawable.pill1), contentDescription = null)
-        Image(painter = painterResource(id = R.drawable.pill2), contentDescription = null)
-        Image(painter = painterResource(id = R.drawable.pill3), contentDescription = null)
-        Image(painter = painterResource(id = R.drawable.pill4), contentDescription = null)
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Start
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.pill2), contentDescription = null,
+                modifier = Modifier.fillMaxWidth(0.7f),
+                contentScale = ContentScale.Crop
+            )
+        }
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.End
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.pill3), contentDescription = null,
+                modifier = Modifier.fillMaxWidth(0.7f),
+                contentScale = ContentScale.Crop
+            )
+        }
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Start
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.pill4), contentDescription = null,
+                modifier = Modifier.fillMaxWidth(0.7f),
+                contentScale = ContentScale.Crop
+            )
+        }
 
 
         Button(
