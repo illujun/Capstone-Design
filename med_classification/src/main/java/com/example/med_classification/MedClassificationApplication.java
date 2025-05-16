0e07f58be3ebdecac8259d9ea11cfd8f5fdbc45e
jsub1379 <minigun47@naver.com>
2025-05-16 15:41:05 +0900

    1차 업데이트
    기본 구조 확정

---
+++ med_classification/src/main/java/com/example/med_classification/MedClassificationApplication.java
@@ -0,0 +1,13 @@
+package com.example.med_classification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedClassificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedClassificationApplication.class, args);
+	}
+
+}
---
+++ med_classification/src/test/java/com/example/med_classification/MedClassificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.med_classification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedClassificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
