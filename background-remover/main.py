368d23532bd037dc8cfab985ae4e2c70b449874f
coldlapse <vegarian@overcast.kr>
2025-05-19 01:26:59 +0900

    ..

--- background-remover/main.py
+++ background-remover/main.py
@@ -1,7 +1,18 @@
+from rembg import remove
+from PIL import Image
+import io
+
+# 커스텀 모델 경로 설정
 from rembg.session_factory import new_session
+session = new_session(model_name='u2net_custom', model_options={'model_path': 'F:\\GitHub\\Capstone-Design\\models\\u2net_pill_136000.onnx'})
 
-model_path = "F:\\GitHub\\Capstone-Design\\background-remover\\u2net_pill_74000.onnx"
-session = new_session(model_path)
+# 이미지 불러오기
+with open('4.jpg', 'rb') as i:
+    input_data = i.read()
 
-# 모델이 잘 로드되었는지 확인
-print(session)  # 출력에 model_path가 포함되어야 함
+# 배경제거
+output_data = remove(input_data, session=session)
+
+# 저장
+with open('4_nobg_136000.png', 'wb') as o:
+    o.write(output_data)
