dc04238462aa48cff36d03b26aa2cdb970e2e2da
전동원 <jdw7645@naver.com>
2025-05-19 16:37:28 +0900

    RESULT PAGE SETTING

--- Findpill/app/src/main/java/com/example/findpill/ui/component/PillList.kt
+++ Findpill/app/src/main/java/com/example/findpill/ui/component/PillList.kt
@@ -2,12 +2,13 @@ package com.example.findpill.ui.component
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -16,34 +17,31 @@ import coil.compose.rememberAsyncImagePainter
 import com.example.findpill.data.model.PillInfo
 
 @Composable
-fun Pill(pill: PillInfo) {
-    Row(
+fun Pill(pill: PillInfo, onClick: () -> Unit) {
+    Card(
         modifier = Modifier
             .fillMaxWidth()
             .background(MaterialTheme.colorScheme.onSecondary, shape = RoundedCornerShape(12.dp))
-            .padding(16.dp),
-        verticalAlignment = Alignment.CenterVertically
+            .padding(16.dp)
+            .clickable { onClick() },
+        shape = RoundedCornerShape(16.dp)
     ) {
-        Image(
-            painter = rememberAsyncImagePainter(pill.image),
-            contentDescription = null,
-            modifier = Modifier.size(64.dp).padding(end = 12.dp)
-        )
-        Column{
-            Text(pill.name, fontWeight = FontWeight.SemiBold, fontSize = 18.sp, color = MaterialTheme.colorScheme.onSurface)
-            Text(pill.description, fontSize = 14.sp, color = MaterialTheme.colorScheme.onSurface, modifier = Modifier.padding(top = 12.dp))
+        Row(
+            modifier = Modifier.padding(16.dp)
+        ){
+            Image(
+                painter = rememberAsyncImagePainter(pill.image),
+                contentDescription = null,
+                modifier = Modifier.size(80.dp)
+            )
+            Column{
+                Text(pill.name, fontWeight = FontWeight.SemiBold, fontSize = 18.sp, color = MaterialTheme.colorScheme.onSurface)
+                Text(pill.description, fontSize = 14.sp, color = MaterialTheme.colorScheme.onSurface, modifier = Modifier.padding(top = 12.dp))
+            }
         }
     }
 }
 
-@Composable
-fun PillTestList() {
-    Column {
-        dummyPillList.forEach {
-            Pill(pill = it)
-        }
-    }
-}
 
 val dummyPillList = listOf(
     PillInfo(
